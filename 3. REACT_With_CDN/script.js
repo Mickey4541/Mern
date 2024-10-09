a13a637b3ae3a79e647eeb9ded7019e4c2205fce
mickey4541 <rajanbhandari4541@gmail.com>
2024-10-09 17:48:28 +0545

    react with cdn

---
+++ 3. REACT_With_CDN/script.js
@@ -0,0 +1,9 @@
+var h1 = React.createElement('h1',null,"Hello from React");
+
+var parent = document.querySelector("#parent")
+
+var root = ReactDOM.createRoot(parent)
+root.render(h1)
+
+
+// This method using cdn is good but we heve to make a complete structure to work with react in a professional and efficient way.So, lets go:: inside react folder
